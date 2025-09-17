1631e293c84a0584f599eea16a447b2e87a26a8e
Jhon Castro <jhon.castro07@uptc.edu.co>
2025-09-17 16:34:52 -0500

    feat: add EDA visualization functions with distribution, correlation and scatter plots

---
+++ src/eda/__init__.py
@@ -0,0 +1,7 @@
+"""
+Módulo para análisis exploratorio de datos (EDA).
+"""
+
+from .eda_plots import plot_distributions, plot_correlations, plot_scatter_pairs
+
+__all__ = ['plot_distributions', 'plot_correlations', 'plot_scatter_pairs']
